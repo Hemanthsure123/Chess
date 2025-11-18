e19c76aa630e6b46728d791d86a9e7689a27f7a8
Hemanth123 <hemanthsure3@gmail.com>
2025-11-18 23:54:58 +0530

    day-2

--- ChessLogic/Player.cs
+++ ChessLogic/Player.cs
@@ -6,7 +6,23 @@ using System.Threading.Tasks;
 
 namespace ChessLogic
 {
-    internal class Player
+    public enum Player
     {
+        None,
+        White,
+        Black
     }
-}
+
+    public static class PlayerExtensions
+    {
+        public static Player Opponent(this Player player)
+        {
+            return player switch
+            {
+                Player.White => Player.Black,
+                Player.Black => Player.White,
+                _ => Player.None,
+            };
+        }
+    }
+} 
--- ChessUI/obj/Debug/net8.0-windows/ChessUI.AssemblyInfo.cs
+++ ChessUI/obj/Debug/net8.0-windows/ChessUI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ChessUI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2ee85e6b7b54740a7d29de004f7c697e8f6ea1b5")]
 [assembly: System.Reflection.AssemblyProductAttribute("ChessUI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ChessUI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
